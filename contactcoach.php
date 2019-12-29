fd8440804ccc719fc545b58d597b5e757e195290
freddymatime <freddy.h.matime@gmail.com>
2019-12-29 20:49:41 +0100

    css + jpeg

--- contactcoach.php
+++ contactcoach.php
@@ -7,11 +7,11 @@
 
     
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="style2.css">
     <title>Resultat trouver un coach</title>
   </head>
   <body>
-
+<h2>
 <div class="card">
 
     <h2 class="card-header info-color white-text text-center py-4">
@@ -26,14 +26,16 @@
 
             
             <div class="md-form mt-3">
+            <label for="materialContactFormName">Name</label>
                 <input type="text" id="materialContactFormName" class="form-control">
-                <label for="materialContactFormName">Name</label>
+                
             </div>
 
             
             <div class="md-form">
+            <label for="materialContactFormEmail">E-mail</label>
                 <input type="email" id="materialContactFormEmail" class="form-control">
-                <label for="materialContactFormEmail">E-mail</label>
+                
             </div>
 
         
@@ -48,11 +50,12 @@
 
             
             <div class="md-form">
-                <textarea id="materialContactFormMessage" class="form-control md-textarea" rows="3"></textarea>
                 <label for="materialContactFormMessage">Message</label>
+                <textarea id="materialContactFormMessage" class="form-control md-textarea" rows="3"></textarea>
+                
             </div>
 
-            <!-- Copy -->
+            
             <div class="form-check">
                 <input type="checkbox" class="form-check-input" id="materialContactFormCopy">
                 <label class="form-check-label" for="materialContactFormCopy">Send me a copy of this message</label>
@@ -67,6 +70,7 @@
     </div>
 
 </div>
+</h2>
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
